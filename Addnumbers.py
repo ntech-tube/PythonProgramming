90c96d4b0eca027ad9fbc4a3e1ffcc6f079e960a
NTech-Tube <65553026+ntech-tube@users.noreply.github.com>
2020-05-22 01:42:28 +0600

    Addnumbers
    how to add two numbers using python

---
+++ Addnumbers.py
@@ -0,0 +1,7 @@
+number_1 = 12
+number_2 = 23
+
+# add two numbers and store in result variable
+result = number_1 + number_2
+
+print("Number 1={0} Number 2={1} Result ={2} ".format(number_1, number_2, result))
